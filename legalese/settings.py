964a7408fef57dfb2f506b8b7a92ca3f354af2f8
Andrei Avram <andrei.avram@gmail.com>
2015-09-21 18:09:18 +0300

    added some static settings

--- legalese/settings.py
+++ legalese/settings.py
@@ -84,6 +84,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "legalese", "static")
 TEMPLATE_DIRS = global_settings.TEMPLATE_DIRS + (os.path.join(BASE_DIR, "templates"), )
 
 STATICFILES_DIRS = (
