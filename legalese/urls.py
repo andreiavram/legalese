003ec21f67918b934d9c72686d3656c7c23ce4cc
Andrei Avram <andrei.avram@gmail.com>
2015-09-21 18:05:16 +0300

    fixed regression

--- legalese/urls.py
+++ legalese/urls.py
@@ -5,7 +5,7 @@ from documents.views import DocumentList, DocumentDetail, NodeDetail
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^document/list/$', DocumentList.as_view(), name="document_list"),
-    url(r'^document/(?P<slug>[-_\w]i-am +)/$', DocumentDetail.as_view(), name="document_detail"),
+    url(r'^document/(?P<slug>[-_\w]+)/$', DocumentDetail.as_view(), name="document_detail"),
     url(r'^node/(?P<pk>\d+)/$', NodeDetail.as_view(), name="node_detail"),
     url(r'^$', DocumentList.as_view(), name="index"),
 )
